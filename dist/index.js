d779a789110d01716be5b171282ce5f9c2b71f4e
pqx <l.r.slegt@gmail.com>
2022-06-15 09:13:38 +0200

    initial

---
+++ dist/index.js
@@ -0,0 +1,47 @@
+"use strict";
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'hello'];
+let person = [1, 'Brad', true];
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+];
+let pid;
+pid = '22';
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
+let cid = 1;
+let customerId = cid;
+function addNum(x, y) {
+    return x + y;
+}
+function log(message) {
+    console.log(message);
+}
+;
+const user1 = {
+    id: 1,
+    name: 'John',
+};
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+// Basic Types
+let id: number = 5;
+let company: string = 'Traversy Media';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'hello'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true];
+// rtuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill'],
+];
+
+// Union
+let pid: string | number;
+
+pid = '22';
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: 'John',
+};
+
+// Type Assertion
+let cid: any = 1;
+//let customerId = <number>cid
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+// Void
+function log(message: string | number): void {
+  console.log(message);
+}
+
+// Interfaces
+interface UserInterface {
+  id: number;
+  name: string;
+};
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+};
